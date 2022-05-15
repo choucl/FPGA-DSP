1ec2c28028436a3a293931256411816e6f1e5520
jumha <e24076750@gs.ncku.edu.tw>
2022-05-15 22:07:02 +0800

    Add def.v

---
+++ Problem1/src/hdl/def.v
@@ -0,0 +1,19 @@
+`ifndef _DEF_
+`define _DEF_
+
+`define EXEC 31
+`define ALUMODE 30:27
+`define OPMODE 26:20
+`define INMODE 19:15
+`define BRAM1_W_ADDR 14:10
+`define BRAM1_R_ADDR 9:5
+`define BRAM0_R_ADDR 4:0
+
+`define ALUMODE_WIDTH 4
+`define OPMODE_WIDTH 7
+`define INMODE_WIDTH 5
+`define ADDR_WIDTH 5
+
+`define I_WIDTH 32
+
+`endif
