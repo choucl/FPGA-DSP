46f67fd32e8fa3e60ebfb7970c23669e08488b53
choucl <clc25600@gmail.com>
2022-05-19 21:16:26 +0800

    Modify main.c for address correctness

--- Problem1/src/main.c
+++ Problem1/src/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "xil.printf.h"
+#include "xil_printf.h"
 #include "xil_io.h"
 #include "xparameters.h"
 
@@ -9,11 +9,11 @@ u32 get_cmd(
         u32 bram1_w_addr,
         u32 inmode,
         u32 opmode,
-        u32 alumode) 
+        u32 alumode)
 {
-    u32 cmd = bram0_r_addr;
-    cmd += bram1_r_addr << 5;
-    cmd += bram1_w_addr << 10;
+    u32 cmd = bram0_r_addr * 4;
+    cmd += (bram1_r_addr * 4) << 5;
+    cmd += (bram1_w_addr * 4) << 10;
     cmd += inmode << 15;
     cmd += opmode << 20;
     cmd += alumode << 27;
@@ -22,22 +22,29 @@ u32 get_cmd(
 }
 
 void execute(u32 cmd) {
-    Xil_Out32(XPAR_AXI_GPIO_0_BASEADDR, cmd); 
-    Xil_Out32(XPAR_AXI_GPIO_0_BASEADDR, 1);  // START 
+    Xil_Out32(XPAR_AXI_GPIO_0_BASEADDR, cmd);
+    Xil_Out32(XPAR_AXI_GPIO_1_BASEADDR, 1);  // START
     while (!Xil_In32(XPAR_AXI_GPIO_2_BASEADDR));  // wait for valid
-    Xil_Out32(XPAR_AXI_GPIO_0_BASEADDR, 0);  // CLEAR 
+    printf("P: %d\n", Xil_In32(XPAR_AXI_GPIO_3_BASEADDR));
+    Xil_Out32(XPAR_AXI_GPIO_1_BASEADDR, 0);  // CLEAR
     return;
 }
 
 int main() {
     u32 cmd = 0;
-    
+    printf("print0--------------------------------------------\n");
+    for (int i = 0; i < 32; ++i) {
+            u32 bram_read;
+            bram_read = Xil_In32(XPAR_AXI_BRAM_CTRL_1_S_AXI_BASEADDR + i * 4);
+            printf("BRAM1[%d] = 0x%x\n", i, bram_read);
+    }
+
     // BRAM[3] <= BRAM0[0] * BRAM1[2]
     execute(get_cmd(0, 2, 3, 0b10001, 0b0000101, 0b0000));
-    
+
     // BRAM[7] <= BRAM0[11] * BRAM1[3]
     execute(get_cmd(11, 3, 7, 0b10001, 0b0000101, 0b0000));
-    
+
     // BRAM[10] <= BRAM0[31] * BRAM1[7] + C
     execute(get_cmd(31, 7, 10, 0b10001, 0b0110101, 0b0000));
 
@@ -47,22 +54,23 @@ int main() {
     // BRAM[15] <= BRAM[0] * BRAM[1] - C - 1
     execute(get_cmd(0, 1, 15, 0b10001, 0b0110101, 0b0001));
 
+    printf("print1--------------------------------------------\n");
     for (int i = 0; i < 32; ++i) {
         u32 bram_read;
-        bram_read = Xil_In32(XPAR_AXI_GPIO_1_BASEADDR + i);
+        bram_read = Xil_In32(XPAR_AXI_BRAM_CTRL_1_S_AXI_BASEADDR + i * 4);
         printf("BRAM1[%d] = 0x%x\n", i, bram_read);
     }
 
     for (int i = 0; i < 32; ++i) {
-        Xil_Out32(XPAR_AXI_BRAM_CTRL_0_S_AXI_BASEADDR + i, (i + 1) ^ 2);
+        Xil_Out32(XPAR_AXI_BRAM_CTRL_0_S_AXI_BASEADDR + i * 4, (i + 1) ^ 2);
     }
-    
+
     // BRAM[16] <= BRAM0[0] * BRAM1[2]
     execute(get_cmd(0, 2, 16, 0b10001, 0b0000101, 0b0000));
-    
+
     // BRAM[17] <= BRAM0[11] * BRAM1[3]
     execute(get_cmd(11, 3, 17, 0b10001, 0b0000101, 0b0000));
-    
+
     // BRAM[18] <= BRAM0[31] * BRAM1[7] + C
     execute(get_cmd(31, 7, 18, 0b10001, 0b0110101, 0b0000));
 
@@ -72,9 +80,10 @@ int main() {
     // BRAM[20] <= BRAM[0] * BRAM[1] - C - 1
     execute(get_cmd(0, 1, 20, 0b10001, 0b0110101, 0b0001));
 
+    printf("print2--------------------------------------------\n");
     for (int i = 0; i < 32; ++i) {
         u32 bram_read;
-        bram_read = Xil_In32(XPAR_AXI_GPIO_1_BASEADDR + i);
+        bram_read = Xil_In32(XPAR_AXI_BRAM_CTRL_1_S_AXI_BASEADDR + i * 4);
         printf("BRAM1[%d] = 0x%x\n", i, bram_read);
     }
     return 0;
